d1d69716dc68d29e715f04c8f181644ef4f93224
Akash Acharya <109788904+AkashAcharya03@users.noreply.github.com>
2025-01-29 15:42:03 +0530

    Time: 0 ms (100%), Space: 17.8 MB (35.64%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -4,16 +4,12 @@ class Solution:
         result=[]
         for i in range(len(digits)-1,-1,-1):
             if i==len(digits)-1:
-
                 sum=digits[i]+1+carry
                 carry=0
-                print("vjbbdv")
             else:
                 sum=digits[i]+carry
                 carry=0
             if sum>9:
-                print("vjbbdv")
-
                 carry=sum//10
                 result.append(sum%10)
             else:
